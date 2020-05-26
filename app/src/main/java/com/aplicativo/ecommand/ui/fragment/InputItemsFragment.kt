6a0cf92af1ac09f489918d1f33b743574e460070
kaleb.silva <kaleb.silva@itbam.org.br>
2020-05-26 16:57:26 -0400

    create dialog to input item

--- app/src/main/java/com/aplicativo/ecommand/ui/fragment/InputItemsFragment.kt
+++ app/src/main/java/com/aplicativo/ecommand/ui/fragment/InputItemsFragment.kt
@@ -1,11 +1,13 @@
 package com.aplicativo.ecommand.ui.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.aplicativo.ecommand.R
+import kotlinx.android.synthetic.main.fragment_input_items.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +21,19 @@ class InputItemsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_input_items, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        floatingActionButtonAddItem.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            val layoutInflater = requireActivity().layoutInflater
+            val view = layoutInflater.inflate(R.layout.input_item_dialog, null)
+
+            builder.setView(view)
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 
 }
